dae84805f79a31abb7dfacbbf35228fc00b2c9b4
pamAdmin <>
2020-06-17 21:25:17 -0500

    {/src/main/java/com/samplespace/rulesample/Loan.java}

--- src/main/java/com/samplespace/rulesample/Loan.java
+++ src/main/java/com/samplespace/rulesample/Loan.java
@@ -6,10 +6,99 @@ package com.samplespace.rulesample;
 
 public class Loan implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Loan() {
-    }
+	@org.kie.api.definition.type.Label("Amount")
+	private double amount;
+	@org.kie.api.definition.type.Label("Duration")
+	private Integer duration;
+
+	@org.kie.api.definition.type.Label(value = "Interest Rate")
+	private java.lang.Double interestRate;
+
+	@org.kie.api.definition.type.Label(value = "Monthly Repayment")
+	private java.lang.Double monthlyRepayment;
+
+	@org.kie.api.definition.type.Label(value = "Age PreQualification")
+	private java.lang.Boolean agePreQual;
+
+	@org.kie.api.definition.type.Label(value = "Loan PreQualification")
+	private java.lang.Boolean loanPreQualification;
+
+	@org.kie.api.definition.type.Label(value = "comment")
+	private java.lang.String comment;
+
+	public Loan() {
+	}
+
+	public double getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public java.lang.Integer getDuration() {
+		return this.duration;
+	}
+
+	public void setDuration(java.lang.Integer duration) {
+		this.duration = duration;
+	}
+
+	public java.lang.Double getInterestRate() {
+		return this.interestRate;
+	}
+
+	public void setInterestRate(java.lang.Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public java.lang.Double getMonthlyRepayment() {
+		return this.monthlyRepayment;
+	}
+
+	public void setMonthlyRepayment(java.lang.Double monthlyRepayment) {
+		this.monthlyRepayment = monthlyRepayment;
+	}
+
+	public java.lang.Boolean getAgePreQual() {
+		return this.agePreQual;
+	}
+
+	public void setAgePreQual(java.lang.Boolean agePreQual) {
+		this.agePreQual = agePreQual;
+	}
+
+	public java.lang.Boolean getLoanPreQualification() {
+		return this.loanPreQualification;
+	}
+
+	public void setLoanPreQualification(java.lang.Boolean loanPreQualification) {
+		this.loanPreQualification = loanPreQualification;
+	}
+
+	public java.lang.String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(java.lang.String comment) {
+		this.comment = comment;
+	}
+
+	public Loan(double amount, java.lang.Integer duration,
+			java.lang.Double interestRate, java.lang.Double monthlyRepayment,
+			java.lang.Boolean agePreQual,
+			java.lang.Boolean loanPreQualification, java.lang.String comment) {
+		this.amount = amount;
+		this.duration = duration;
+		this.interestRate = interestRate;
+		this.monthlyRepayment = monthlyRepayment;
+		this.agePreQual = agePreQual;
+		this.loanPreQualification = loanPreQualification;
+		this.comment = comment;
+	}
 
 
 
